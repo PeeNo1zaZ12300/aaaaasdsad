17f7ee5bbe076884fd4fdd09466e2fa0f8686f81
PeeNo1zaZ12300 <86155546+PeeNo1zaZ12300@users.noreply.github.com>
2021-08-05 22:05:43 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include 'uidfreefire.class.php';
 //key antirecaptcha ไปหาซื้อกันเองนะครัชชช
 $token_antirecaptcha = "ODcwNTA4NDQ4MTU5MjU2NjE2.YQNyKw.E5o6MzQUOGLtyAepH4XC0ZL4gJU";
 $topup = New topup_freefire($token_antirecaptcha);
-$uid = '950516437';
+$uid = '2469348040';
 //login ด้วย uid freefire ก่อน
 print_r($topup->login_freefire($uid));
 //login เสร็จแล้วจะได้open_idไว้เติมฟรีฟายด้วยบัตรการีน่า
